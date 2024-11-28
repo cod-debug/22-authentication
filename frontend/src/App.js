e724b20aab95ad0d3bdb0db695965b34bbe784c9
Roy Duenas <rduenas@ingenuiti.com>
2024-11-28 20:59:40 +0800

    New learnings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,13 +13,17 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthenticationPage from './pages/Authentication';
+import AuthenticationPage, { action as authAction } from './pages/Authentication';
+import { action as logoutAction } from './pages/Logout';
+import { tokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
@@ -62,7 +66,12 @@ const router = createBrowserRouter([
       },
       {
         path: 'auth',
+        action: authAction,
         element: <AuthenticationPage />
+      },
+      {
+        path: 'logout',
+        action: logoutAction
       }
     ],
   },
